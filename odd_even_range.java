e4cf43af02356dbf600fabf361bd72b3ea12a4cd
Gaurav Sablok <sablokg@gmail.com>
2023-02-27 18:53:06 +0530

    Add files via upload

---
+++ odd_even_range.java
@@ -0,0 +1,36 @@
+// java scanner module
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter_the_number:");
+        int num = input.nextInt();
+           for (num=1; num<10; num+=2)
+            {
+                System.out.println(num);
+            }
+        }
+
+        ;
+    }
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter_the_number:");
+        int num = input.nextInt();
+        for (num=0; num<10; num+=2)
+        {
+            System.out.println(num);
+        }
+    }
+
+    ;
+}
+
+
+
+
+// java string
+
+
+
